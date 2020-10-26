0af488e35e3585e32f53ed2a5ef213ca950f0068
Anatol <67501086+xolesteroll@users.noreply.github.com>
2020-10-26 10:42:58 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,49 +1,50 @@
 function sliderInit(slides, prev, next, slidesWindow, slidesField, marginLeft, slidesWindowWidth, offset, slidesToShow, slidesToScroll = 1) {
-    offset = 0;
-    slidesField.style.width = 100 * slides.length + '%';
-
-    slides.forEach(slide => {
-        if (marginLeft != 0) {
-            slide.style.width = (slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow  + 'px';
-        } else {
-            slide.style.width = slidesWindowWidth / slidesToShow + 'px';
-        }
-    });
-
-    next.addEventListener('click', () => {
-        if (marginLeft != 0) {
-            if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
-                offset = 0;
+        offset = 0;
+        slidesField.style.width = ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) * (slides.length / slidesToShow)) + (marginLeft * (slides.length - 1)) + 'px';
+    
+        slides.forEach(slide => {
+            if (marginLeft != 0) {
+                slide.style.width = (slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow  + 'px';
             } else {
-                offset += ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow) * slidesToScroll + (marginLeft * slidesToScroll);
+                slide.style.width = slidesWindowWidth / slidesToShow + 'px';
             }
-            console.log(offset);
-        } else {
-            if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
-                offset = 0;
+        });
+    
+        next.addEventListener('click', () => {
+            if (marginLeft != 0) {
+                if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
+                    offset = 0;
+                } else {
+                    offset += ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow) * slidesToScroll + (marginLeft * slidesToScroll);
+                }
+                console.log(offset);
             } else {
-                offset += slidesWindowWidth / slidesToShow * slidesToScroll;
+                if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
+                    offset = 0;
+                } else {
+                    offset += slidesWindowWidth / slidesToShow * slidesToScroll;
+                }
             }
-        }
-        slidesField.style.transform = `translateX(-${offset}px)`;
-
-    });
-
-    prev.addEventListener('click', () => {
-        if (marginLeft != 0) {
-            if (offset == 0) {
-                offset = (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1));
+            slidesField.style.transform = `translateX(-${offset}px)`;
+    
+        });
+    
+        prev.addEventListener('click', () => {
+            if (marginLeft != 0) {
+                if (offset == 0) {
+                    offset = (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1));
+                } else {
+                    offset -= ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / (slidesToShow)) * slidesToScroll + (marginLeft * slidesToScroll);
+                }
             } else {
-                offset -= ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / (slidesToShow)) * slidesToScroll + (marginLeft * slidesToScroll);
+                if (offset == 0) {
+                    offset = slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1);
+                } else {
+                    offset -= slidesWindowWidth / slidesToShow * slidesToScroll;
+                }
             }
-        } else {
-            if (offset == 0) {
-                offset = slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1);
-            } else {
-                offset -= slidesWindowWidth / slidesToShow * slidesToScroll;
-            }
-        }
-        slidesField.style.transform = `translateX(-${offset}px)`;
-
-    });
+            slidesField.style.transform = `translateX(-${offset}px)`;
+    
+        });
 }
+
