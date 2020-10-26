2694aedee39640043d1c4057ca0881ce42de0bd9
Anton <Iarovoi>
2020-10-26 16:49:06 +0200

    added data- attributes to slider

--- slider.js
+++ slider.js
@@ -1,49 +1,63 @@
-function sliderInit(slides, prev, next, slidesWindow, slidesField, marginLeft, slidesWindowWidth, offset, slidesToShow, slidesToScroll = 1) {
-    offset = 0;
-    slidesField.style.width = 100 * slides.length + '%';
 
-    slides.forEach(slide => {
-        if (marginLeft != 0) {
-            slide.style.width = (slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow  + 'px';
-        } else {
-            slide.style.width = slidesWindowWidth / slidesToShow + 'px';
-        }
-    });
 
-    next.addEventListener('click', () => {
-        if (marginLeft != 0) {
-            if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
-                offset = 0;
+    // Slider Init Function
+    function sliderInit(slider, slidesToShow = 1, slidesToScroll = 1) {
+        const slides = slider.querySelectorAll('[data-slider-slide]'),
+        prev = slider.querySelector('[data-slider-prev]'),
+        next = slider.querySelector('[data-slider-next]'),
+        slidesWindow = slider.querySelector('[data-slider-wrapper]'),
+        slidesField = slider.querySelector('[data-slider-inner]'),
+        marginLeft = parseInt(window.getComputedStyle(slides[1]).marginLeft),
+        slidesWindowWidth = parseInt(window.getComputedStyle(slidesWindow).width);
+        
+        let offset = 0;
+
+        slidesField.style.width = ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) * (slides.length / slidesToShow)) + (marginLeft * (slides.length - 1)) + 'px';
+
+        
+        let slidesFieldWidth = parseInt(window.getComputedStyle(slidesField).width);
+    
+        slides.forEach(slide => {
+            if (marginLeft != 0) {
+                slide.style.width = (slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow  + 'px';
             } else {
-                offset += ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow) * slidesToScroll + (marginLeft * slidesToScroll);
+                slide.style.width = slidesWindowWidth / slidesToShow + 'px';
             }
-            console.log(offset);
-        } else {
-            if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
-                offset = 0;
+        });
+    
+        next.addEventListener('click', () => {
+            if (marginLeft != 0) {
+                if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
+                    offset = 0;
+                } else {
+                    offset += ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / slidesToShow) * slidesToScroll + (marginLeft * slidesToScroll);
+                }
             } else {
-                offset += slidesWindowWidth / slidesToShow * slidesToScroll;
+                if (offset >= (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1))) {
+                    offset = 0;
+                } else {
+                    offset += slidesWindowWidth / slidesToShow * slidesToScroll;
+                }
             }
-        }
-        slidesField.style.transform = `translateX(-${offset}px)`;
-
-    });
-
-    prev.addEventListener('click', () => {
-        if (marginLeft != 0) {
-            if (offset == 0) {
-                offset = (slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1));
+            slidesField.style.transform = `translateX(-${offset}px)`;
+    
+        });
+    
+        prev.addEventListener('click', () => {
+            if (marginLeft != 0) {
+                if (offset == 0) {
+                    offset = slidesFieldWidth - slidesWindowWidth;
+                } else {
+                    offset -= ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / (slidesToShow)) * slidesToScroll + (marginLeft * slidesToScroll);
+                }
             } else {
-                offset -= ((slidesWindowWidth - (marginLeft * (slidesToShow - 1))) / (slidesToShow)) * slidesToScroll + (marginLeft * slidesToScroll);
+                if (offset == 0) {
+                    offset = slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1);
+                } else {
+                    offset -= slidesWindowWidth / slidesToShow * slidesToScroll;
+                }
             }
-        } else {
-            if (offset == 0) {
-                offset = slidesWindowWidth * ((Math.round(slides.length) / slidesToShow) - 1);
-            } else {
-                offset -= slidesWindowWidth / slidesToShow * slidesToScroll;
-            }
-        }
-        slidesField.style.transform = `translateX(-${offset}px)`;
-
-    });
-}
+            slidesField.style.transform = `translateX(-${offset}px)`;
+    
+        });
+    }
